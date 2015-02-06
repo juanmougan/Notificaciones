8f9432dec0b32181d2bf5f21d3b2fed74f4ca7e2
Juan Manuel Mougan <juanmougan@gmail.com>
2015-02-06 20:52:42 -0300

    Se empieza con la settings activity

---
+++ app/src/main/java/ar/edu/uca/ingenieria/notificaciones/config/SettingsActivity.java
@@ -0,0 +1,67 @@
+package ar.edu.uca.ingenieria.notificaciones.config;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ar.edu.uca.ingenieria.notificaciones.R;
+
+public class SettingsActivity extends Activity {
+
+    // TODO ademas del fragment, agregar botones?
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Este Fragment contiene la configuración.
+     */
+    public static class SettingsFragment extends PreferenceFragment {
+
+        public SettingsFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // addPreferencesFromResource();
+        }
+    }
+}
